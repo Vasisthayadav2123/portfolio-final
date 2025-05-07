7b73fe24732145f2533154de2a0257359c5fd8b9
Vasisthayadav2123 <vasisthayadav58@gmail.com>
2025-05-07 17:15:40 +0530

    loading screen and basic loading transitions

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-
+import { LoadingScreen } from './components/loadingscreen'
 import './App.css'
+import './index.css'
+import { useState } from 'react'
+import { Nav } from './components/nav'
 
 function App() {
 
+  const [isLoaded , setIsLoaded] = useState(false);
+
   return (
     <>
-      
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+        <div className={` min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100 `}>
+          <Nav />
+        </div>
     </>
   )
 }
