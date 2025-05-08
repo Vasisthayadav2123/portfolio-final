ac01b09fb7d7dddc4ecb44f2da47ddd83effd98f
Vasisthayadav2123 <vasisthayadav58@gmail.com>
2025-05-08 16:33:56 +0530

    all sections except contact me complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,34 @@
-import { LoadingScreen } from './components/loadingscreen'
-import './App.css'
-import './index.css'
-import { useState } from 'react'
-import { Nav } from './components/nav'
-import { MobileNav } from './components/mobileNav'
+import { LoadingScreen } from './components/loadingscreen';
+import './App.css';
+import './index.css';
+import { useState } from 'react';
+import { Nav } from './components/nav';
+import { MobileNav } from './components/mobileNav';
+import { About } from './components/section/about';
+import { Home } from './components/section/home';
+import { Projects } from './components/section/projects';
+
 
 function App() {
-
-  const [isLoaded , setIsLoaded] = useState(false);
-  const [menuOpen , setMenuOpen] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <>
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
-        <div className={` min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100 `}>
-          <Nav  menuOpen={menuOpen} setMenuOpen = {setMenuOpen} />
-          <MobileNav menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-        </div>
+      <div
+        className={`min-h-screen transition-opacity duration-700 ${
+          isLoaded ? 'opacity-100' : 'opacity-0'
+        } bg-black text-gray-100`}
+      >
+        <Nav menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <MobileNav menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <Home />
+        <About />
+        <Projects />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
